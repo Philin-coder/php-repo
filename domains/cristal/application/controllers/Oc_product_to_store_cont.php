b53b2d52c1eb64ac82376b7ef2068d811d985120
Alexey <alexey.user.1988@gmail.com>
2022-02-23 22:47:30 +0300

    4

--- domains/cristal/application/controllers/Oc_product_to_store_cont.php
+++ domains/cristal/application/controllers/Oc_product_to_store_cont.php
@@ -3,7 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Oc_product_to_store_cont extends CI_Controller {
     public function index()
     {
-        $this->load->
         $this->load->view('head');
         $this->load->view('header');
         $this->load->view('view_prod_to_store');
