8b8102371d7834d35ce6c3145e1636d79963b23d
Alexey <alexey.user.1988@gmail.com>
2022-02-23 20:40:31 +0300

    подключенные виды

--- domains/cristal/application/controllers/Oc_product_to_store_cont.php
+++ domains/cristal/application/controllers/Oc_product_to_store_cont.php
@@ -3,7 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Oc_product_to_store_cont extends CI_Controller {
     public function index()
     {
-        $this->load->view('view_crystal');
+        $this->load->
+        $this->load->view('head');
+        $this->load->view('header');
+        $this->load->view('view_prod_to_store');
+        $this->load->view('footer');
     }
 }
 ?>
