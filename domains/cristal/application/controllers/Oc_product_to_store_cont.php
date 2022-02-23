24d5a6b4f5262ae9a753a02481b00d5cd887300b
Alexey <alexey.user.1988@gmail.com>
2022-02-23 13:12:49 +0300

    заготовки под контроллеры

---
+++ domains/cristal/application/controllers/Oc_product_to_store_cont.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Oc_product_to_store_cont extends CI_Controller {
+    public function index()
+    {
+        $this->load->view('view_crystal');
+    }
+}
+?>
---
+++ domains/cristal/application/controllers/Oс_product_cont.php
@@ -0,0 +1,10 @@
+    <?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+        class Oc_product_cont extends CI_Controller{
+            public function index()
+            {
+                $this->load->view('view_crystal');
+            }
+
+        }
+    ?>
