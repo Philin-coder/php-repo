ee3a7afeb6984bb51775a4aca88b65e2f87af7b1
Alexey <alexey.user.1988@gmail.com>
2022-03-14 22:46:11 +0300

    добавлен фал hatches проверю redirect

--- domains/cristal/application/views/view_test.php
+++ domains/cristal/application/views/view_test.php
@@ -1,7 +1,11 @@
 <body>
     <div class="content">
-<button type="submit" onclick="window.location.reload("http://cristal:8080/index.php/oc_product_image_cont/index");">  Импорт в xml</button>
 
+
+    
+
+<button type="submit"">  Импорт в xml</button>
 </div>
+
 
 </body>
