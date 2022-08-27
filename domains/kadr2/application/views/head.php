e169ce012e4a25e449f76e662ea90f32da899641
Alexey <alexey.user.1988@gmail.com>
2022-08-27 19:07:40 +0300

    Файлы

---
+++ domains/kadr2/application/views/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>РеКадр</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap4.3.1.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
