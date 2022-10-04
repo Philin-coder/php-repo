88e0088334739c18e951ad58758fcdc2caedab80
Alexey <alexey.user.1988@gmail.com>
2022-10-04 12:32:15 +0300

    main_con

--- domains/stipmvc/application/views/index.php
+++ domains/stipmvc/application/views/index.php
@@ -1,13 +1,4 @@
-<?php
-/*
-include 'temp/head.php';
-include 'temp/navbar.php';
-include 'temp/header.php';
 
-$mysqli=new mysqli ("localhost", "root", "","stip2");
-$mysqli->set_charset("utf8");
-*/
-?>
 <div class="container">
     <div class="row">
 <div class="col">
@@ -29,17 +20,6 @@ $mysqli->set_charset("utf8");
     </div>
 </div>
 </div>
-<?php
-/*
-if (!empty($_POST))
-{
-    $name_grup=$_POST['name_grup'];
-    $spec=$_POST['spec'];
-    $sql=$mysqli->query("insert into gruppa(name_grup, spec) values ('$name_grup','$spec')");
-    $result=$mysqli->query($sql);
-}
-*/
-?>
 <div class="container">
     <div class="row">
         <table class="table">
@@ -49,22 +29,18 @@ if (!empty($_POST))
                 <th>Специальнотсь</th>
             </tr>
             <?php
-            /*
-$result=$mysqli->query("select * from gruppa");
-while ($row=$result->fetch_array())
-{
+
+if (isset($gruppa)){
+foreach($gruppa as $row):{
 echo '<tr>
 <td>'.$row['id_grupp'].'</td>
 <td>'.$row['name_grup'].'</td>
 <td>'.$row['spec'].'</td>
 </tr>';
 }
-    $result->free();
-    $mysqli->close();
-    */
-
-            ?>
-        </table>
-    </div>
-</div>
-
+endforeach;
+echo('</table>');
+echo('</div>');
+echo('</div>');
+}
+?>
