c3ba17c6ed1924c13df9b6c73637bddea6b97211
Alexey <alexey.user.1988@gmail.com>
2022-10-08 00:03:32 +0300

    kirpich

---
+++ domains/kirpich/temp/head.php
@@ -0,0 +1,11 @@
+ <!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jquery/jquery-3.3.1.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Главная</title>
+</head>
