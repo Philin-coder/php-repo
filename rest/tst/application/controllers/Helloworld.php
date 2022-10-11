69c51dafa1e10ae8d3f1be2e080268c14a9c02eb
Alexey <alexey.user.1988@gmail.com>
2022-10-11 19:39:15 +0300

    fix

---
+++ rest/tst/application/controllers/Helloworld.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+class Helloworld extends  CI_Controller
+{
+    public function index()
+    {
+        echo 'Hello World!';
+        //http://tst:8080/index.php/Helloworld/index
+    }
+}
