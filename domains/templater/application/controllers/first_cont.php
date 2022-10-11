0a5ac0fc9e9e2ceb00c08d7d8461d077902cef78
Alexey <alexey.user.1988@gmail.com>
2022-10-11 23:16:11 +0300

    fix links

--- domains/templater/application/controllers/first_cont.php
+++ domains/templater/application/controllers/first_cont.php
@@ -4,7 +4,7 @@ class first_cont extends CI_Controller {
     public function index(){
         $this->load->helper('url');
         $this->load->view('temp/head');
-        $this->load->view('temp/header');
+        $this->load->view('temp/navbar');
         $this->load->view('index');
         $this->load->view('temp/footer');
         $this->load->view('temp/scripter');
