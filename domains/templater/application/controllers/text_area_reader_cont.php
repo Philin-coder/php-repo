83272da60da4eb7bb693a3536743d2cb95f4bf2a
Alexey <alexey.user.1988@gmail.com>
2022-10-15 00:09:16 +0300

    load fix

--- domains/templater/application/controllers/text_area_reader_cont.php
+++ domains/templater/application/controllers/text_area_reader_cont.php
@@ -1,10 +1,13 @@
 <?php 
 if (! defined ('BASEPATH')) EXIT ('No direct script access aliwed');
 class text_area_reader_cont extends CI_Controller{
+    public function file_write(){
+        //дописать
+    }
     public function index(){
         $this->load->helper('url');
         $this->load->helper('file');
-        $this->looad->view('temp/head');
+        $this->load->view('temp/head');
         $this->load->view('temp/navbar');
         $this->load->view('abot_film');
         $this->load->view('temp/footer');
