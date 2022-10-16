457e0aa37e3b42207035963cf2fe88fb308d72ee
Alexey <alexey.user.1988@gmail.com>
2022-10-16 12:56:06 +0300

    anotehr calc fix

--- domains/templater/application/controllers/another_counter_cont.php
+++ domains/templater/application/controllers/another_counter_cont.php
@@ -2,11 +2,36 @@
 if (! defined ('BASEPATH')) EXIT ('No direct script access aliwed');
 
 class another_counter_cont extends CI_Controller {
+    public function calc(){
+        if (isset($_POST['num1'])){ 
+            $result=0;
+          $num1=$_POST['num1'];
+          $num2=$_POST['num2'];
+          $sign=$_POST['sign'];
+          switch($sign){
+            case '+':$result=$num1+$num2; break;
+            case '-':$result=$num1-$num2;break;
+            case '*':$result=$num1*$num2;break;
+            case '/':if($num2!=0){$result=$num1/$num2;
+            }else{
+              $result='деление на 0';
+            };
+            break;
+          default:$result='Неизвестное действие';
+          }
+          if($result){
+            return $result;
+          }
+          
+          }
+          
+    }
     public function index(){
         $this->load->helper('url');
         $this->load->view('temp/head');
         $this->load->view('temp/navbar');
-        $this->load->view('another_counter');
+        $data['res']=$this->calc();
+        $this->load->view('another_counter',$data);
         $this->load->view('temp/footer');
         $this->load->view('temp/scripter');
 
