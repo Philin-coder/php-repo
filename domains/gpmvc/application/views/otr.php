88a321a380d6eca24a3470313bfab5e535138da6
Alexey <alexey.user.1988@gmail.com>
2022-10-18 09:54:18 +0300

    clean view

--- domains/gpmvc/application/views/otr.php
+++ domains/gpmvc/application/views/otr.php
@@ -1,29 +1,9 @@
-<?php 
-// session_start();
-// $sms=" "; 
-// $conn = new mysqli('localhost','root','','rekadr');
-// if ($conn->connect_error){ echo ("Ошибка соединения с сервером MySQLI: ").$conn->connect_error."<br>";
-//      die("Соединение установлено не было.");}
-//  //установим кодировку
-// $conn  ->set_charset("utf8");
-// if (!empty($_POST)) 
-// { 
-// $naim_o=$_POST['naim_o']; 
-// // добавление записи
-// $sql =$conn->query ("insert into otrasl(naim_o) values ('$naim_o')");			
-//   $sms = "Отрасль введена !";
-//   header("Location: http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/otr.php"); 
-// } 
-//include 'temp/head.php';
-//include 'temp/header_a.php';
- ?>
   <div class="slider">
     <div class="container">
      <div class="center wow fadeInDown">
         <h2>Ввод отрасли</h2>
      </div>
        <form method="POST" action="">
-	   <?//php echo '<div>' .$sms.'</div>' ?>
 	     <div class="form-group row">
     <label for="inputEmail3" class="col-sm-2 col-form-label">Наименование </label>
     <div class="col-sm-10">
--- domains/gpmvc/application/views/soisk.php
+++ domains/gpmvc/application/views/soisk.php
@@ -1,33 +1,4 @@
-<?php 
-//session_start();
-// $sms=" "; 
-// $conn = new mysqli('localhost','root','','rekadr');
-// if ($conn->connect_error){ echo ("Ошибка соединения с сервером MySQLI: ").$conn->connect_error."<br>";
-//      die("Соединение установлено не было.");}
-//  //установим кодировку
-// $conn  ->set_charset("utf8");
-// if (!empty($_POST)) 
-// { 
-// $data_reg=$_POST['data_reg'];
-// $fio_s=$_POST['fio_s'];
-// $data_rog=$_POST['data_rog'];
-// $pol=$_POST['pol'];
-// $gragd=$_POST['gragd']; 
-// $adres_s=$_POST['adres_s'];
-// $tel_s=$_POST['tel_s'];
-// $obraz=$_POST['obraz'];
-// $stag=$_POST['stag'];
-// $spec=$_POST['spec'];
-// $sp=$_POST['sp'];
-// // добавление записи
-// $sql =$conn->query ("insert into soiskatel(data_reg, fio_s, data_rog, pol, gragd, adres_s, tel_s, obraz, stag, spec, sp) values 
-// ('$data_reg', '$fio_s', '$data_rog', '$pol', '$gragd', '$adres_s', '$tel_s', '$obraz', '$stag', '$spec', '$sp')");			
-//   $sms = "Соискатель введен !"; 
-//   header("Location: http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/soisk.php");
-//} 
-// include 'temp/head.php';
-// include 'temp/header.php';
- ?>
+
   <div class="slider">
     <div class="container">
     <div class="center wow fadeInDown">
@@ -36,7 +7,7 @@
     <div class="row">
       <div class="col-lg-12">
     <form method="POST" action="">
-	   <?php //echo '<div>' .$sms.'</div>' ?>
+
 	     <div class="form-group row">
          <label for="inputEmail3" class="col-sm-2 col-form-label">Дата регистрации </label>
        <div class="col-sm-10">
