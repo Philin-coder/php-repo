b02e4246454cd5ab0b1336edd938eb63b2f51120
Alexey <alexey.user.1988@gmail.com>
2022-10-19 13:50:15 +0300

    fix search

--- domains/gpmvc/application/controllers/search_cont.php
+++ domains/gpmvc/application/controllers/search_cont.php
@@ -3,9 +3,10 @@ if (! defined ('BASEPATH')) EXIT ('No direct script access aliwed');
 class search_cont extends CI_Controller {
     public function index(){
         $this->load->helper('url');
+        $this->load->model('rk_searcher_model');
         $this->load->view('temp/head');
         $this->load->view('temp/header_s');
-        $this->load->model('rk_searcher_model');
+        
         if (!empty($_POST)){
             $this->rk_searcher_model->finder($_POST['search']);
             //redirect(base_url().'search_cont/index','refresh');
@@ -13,6 +14,7 @@ class search_cont extends CI_Controller {
         if(!empty($_POST)){
             $data['find']=$this->rk_searcher_model->sel_grid_data($_POST['search']);
             $this->rk_searcher_model->sel_grid_data($_POST['search']);
+            
         }
         //$this->load->view('soisk_s',$data);
         $this->load->view('soisk_s',$data);
