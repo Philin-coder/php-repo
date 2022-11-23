de70a1f7496d2cc6d98ddd4da40006062ede5e0c
Alexey <alexey.user.1988@gmail.com>
2022-11-23 12:18:32 +0300

    reader_model

---
+++ domains/libr2/application/models/mt_reader_model.php
@@ -0,0 +1,33 @@
+<?php
+if (! defined ('BASEPATH')) EXIT ('No direct script access aliwed');
+class mt_reader_model extends CI_Model {
+    public function sel_reader () {
+        $this->db->select('ID_Reader,Name_R,Date_B,Adres,Tel,Date_R');
+        $this->db->from('Reader');
+        $sql=$this->db->get();
+        return $sql->result_array();
+
+    }
+    public function ins_reader($Name_R,$Date_B,$Adres,$Tel,$Date_R) {
+        if (!empty($_POST)){
+            $data=array(
+                'Name_R'=>$Name_R,
+                'Date_B'=>$Date_B,
+                'Adres'=>$Adres,
+                'Tel'=>$Tel,
+                'Date_R'=>$Date_R
+
+            );
+            $this->db->insert('Reader',$data);
+
+        }
+    }
+    public function upd_reader($Name_R,$Date_B,$Adres,$Tel,$Date_R){
+        $sql='update reader set Name_R=?,Date_B=?,Adres=?,$Tel=?,$Date_R=?';
+        $this->db->where($sql,array(Name_R,$Date_B,$Adres,$Tel,$Date_R));
+    }
+    public function del_reader(){
+        $this->db->empty_table('reader');
+    }
+}
+?>
